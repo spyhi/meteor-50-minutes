b40e28fb5486c6af8b49a8a3f03f81cf5fe12b4c
Ed White <ew@spyhi.com>
2016-10-09 23:51:29 -1000

    Task 4: Tasklist authentication

--- app/tasklist.js
+++ app/tasklist.js
@@ -15,17 +15,14 @@ if (Meteor.isClient) {
     "submit .add-task": function(event) {
       var name = event.target.name.value;
 
-      Tasks.insert({
-        name: name,
-        createdAt: new Date()
-      });
+      Meteor.call('addTask', name);
 
       event.target.name.value = '';
       return false;
     },
     "click .delete-task": function(event){
       if(confirm('Delete Task?')){
-        Tasks.remove(this._id);
+        Meteor.call('deleteTask', this._id)
       }
     }
   })
@@ -34,3 +31,19 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
 
 }
+
+Meteor.methods({
+  addTask: function(name){
+    if (!Meteor.userId()) {
+      throw new Meteor.Error('No Access');
+    }
+    Tasks.insert({
+      name: name,
+      createdAt: new Date(),
+      userId: Meteor.userId()
+    });
+  },
+  deleteTask: function(taskId) {
+    Tasks.remove(taskId);
+  }
+});
